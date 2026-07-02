f360952db9e4ca50e345127d40817a0ef7d94ff7
a <a>
2026-07-02 18:21:51 +0200

    no objs

--- .objs/main.d
+++
@@ -1,8 +0,0 @@
-.objs/main.o: srcs/main.c includes/parsing.h includes/coders.h \
-  includes/utils.h
-
-includes/parsing.h:
-
-includes/coders.h:
-
-includes/utils.h:
--- .objs/parsing.d
+++
@@ -1,4 +0,0 @@
-.objs/parsing.o: srcs/parsing/parsing.c \
-  srcs/parsing/../../includes/parsing.h
-
-srcs/parsing/../../includes/parsing.h:
